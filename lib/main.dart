f45ec7d1354aabe9a35fefa84708c5ca4e35111c
Himanshu <charlie05puth@gmail.com>
2023-10-19 18:25:12 +0530

    Working list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/views/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  const Home(),
       title: 'Beats',
       theme: ThemeData(
         appBarTheme: AppBarTheme(
